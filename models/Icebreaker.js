3f82103f59b99f8a81c9f8cf566c04f0cdb43b2c
jencekade <gaephuoc@gmail.com>
2017-11-15 09:47:27 +0000

    automatically backed up by learn

--- models/Icebreaker.js
+++ models/Icebreaker.js
@@ -1,5 +1,6 @@
 'use strict';
-const Question = require('./Questions.js');
+
+const Question = require('./Question.js');
 
 class Icebreaker{
   question(){
@@ -8,4 +9,4 @@ class Icebreaker{
 }
 
 module.exports = Icebreaker;
-//const Question = require('model/Questions.js');
+//const Question = require('model/Question.js');
